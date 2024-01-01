a828d445a9dc87a82952bd13c513cce9773cd0fc
Sarah Cohen <cohen.sarah591@gmail.com>
2024-01-01 04:37:19 +0000

    initial cleanup and navbar work

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { Disclosure } from "@headlessui/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export default function Navbar() {
+
+    let pathname = usePathname() || '/'
+    return (
+        <Disclosure as="nav">
+            {({open}) => (
+                <>
+                <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex justify-between h-16">
+                        <div className="flex justify-between w-full">
+                            <div className="flex items-center">
+                                <Link href="/">
+                                    <h1 className="text-2xl font-medium">
+                                        Sarah <span className="text-pink-500">Cohen</span>
+                                    </h1>
+                                </Link>
+                            </div>
+
+                            <div className="hidden sm:ml-6 sm:flex sm:space-x-8 sm:items-center">
+                                <Link href="/" prefetch className={`${
+                                    pathname === "/" 
+                                    ? "border-pink-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" 
+                                    : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                                }`}
+                                >
+                                    Home
+                                </Link>
+                                {/* <Link href="/guestbook" prefetch className={`${
+                                    pathname === "/guestbook" 
+                                    ? "border-pink-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" 
+                                    : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                                }`}
+                                >
+                                    Guestbook
+                                </Link> */}
+                                <Link href="/projects" prefetch className={`${
+                                    pathname === "/projects" 
+                                    ? "border-pink-500 dark:text-white h-full inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium" 
+                                    : "border-transparent text-gray-500 dark:text-gray-300 dark:hover:text-white inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                                }`}
+                                >
+                                    Projects
+                                </Link>
+                            </div>
+                        </div>
+
+                        <div className="-mr-2 flex items-center sm:hidden">
+                            <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-pink-500 dark:hover:bg-gray-800">
+                                {open ?(
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                                    </svg>
+                                ): (
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                                    </svg>
+
+                                )}
+                            </Disclosure.Button>
+                        </div>
+
+                    </div>
+
+                </div>
+                </>
+            )}
+        </Disclosure>
+    )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Navbar from './components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-white text-black dark:bg-[#090908] dark:text-white h-full selection:bg-gray-50 dark:selection:bg-gray-800`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
