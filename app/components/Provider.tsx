9c0684d271a6f0a43deb15905eb3403bfce02808
Sarah Cohen <cohen.sarah591@gmail.com>
2024-01-01 20:14:12 +0000

    added dark/light mode button functionality

--- app/components/Provider.tsx
+++ app/components/Provider.tsx
@@ -6,5 +6,5 @@ import { ThemeProvider } from "next-themes";
 import { ReactNode } from "react";
 
 export function Provider({children} : {children : ReactNode}) {
-    return <ThemeProvider>{children}</ThemeProvider>
+    return <ThemeProvider attribute="class">{children}</ThemeProvider>
 }
