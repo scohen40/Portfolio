511efd7ead661c0f4ffeef215239bccb7a09a1b6
Sarah Cohen <cohen.sarah591@gmail.com>
2024-01-01 20:49:57 +0000

    added profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Me from "@/public/profilepic.jpg"
 
 export default function Home() {
   return (
-    <h1>Hello from the index page</h1>
+    <div className="divide-y divide-gray-100 dark:divide-gray-700">
+      <div className="space-y-2 pt-5 pb-8 md:space-x-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-13">
+          Home
+        </h1>
+      </div>
+
+      <div className="items-center space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
+        <div className="flex flex-col items-center pt-8">
+          <Image alt="Picture of Sarah Cohen" src={Me} className="h-48 w-48 rounded-full object-cover object-top" />
+        </div>
+      </div>
+
+    </div>
   )
 }
